1acfe5151573275dc7767296a6e081f2b2de918d
saurabhk <saurabhk@a-t-g.com>
2012-07-03 14:02:02 -0500

    scribe OAuth added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,43 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.Group;
+import play.Logger;
+import play.mvc.Controller;
+import play.mvc.Result;
+import service.MongoConnectionFactory;
+import service.RenderTweetResults;
 
-import views.html.*;
+import com.google.code.morphia.Datastore;
+import com.google.code.morphia.mapping.Mapper;
+import com.google.code.morphia.query.UpdateOperations;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+	public static Result index() throws Exception {
+
+		Logger.info("attempting to call renderTweet");
+//		RenderTweetResults.getResult();
+		return ok("Ok.");
+		
+/*		Datastore ds = MongoConnectionFactory.getInstance().getDs();
+
+		Logger.info("ds connected");
+		Group me = new Group();
+		me.groupName = "test group";
+		ds.save(me);
+		Group e3 = ds.find(Group.class).get();
+		UpdateOperations<Group> ops = ds.createUpdateOperations(Group.class)
+				.set("groupName", "hello world");
+
+		ds.update(
+				ds.createQuery(Group.class).field(Mapper.ID_KEY).equal(e3.id),
+				ops);
+		String result = "";
+		for (Group me2 : ds.find(Group.class)) {
+			System.out.println(me2.groupName);
+			result = me2.groupName;
+		}
+		return ok(result);*/
+	}
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,18 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+     "com.google.code.morphia" % "morphia" % "0.99",
+     "org.mongodb" % "mongo-java-driver" % "2.7.3",
+     "com.google.code.morphia" % "morphia-logging-slf4j" % "0.99",
+     "org.scribe" % "scribe" % "1.3.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here     
+     resolvers += "Maven repository" at "http://morphia.googlecode.com/svn/mavenrepo/",
+     resolvers += "MongoDb Java Driver Repository" at "http://repo1.maven.org/maven2/org/mongodb/mongo-java-driver/",
+     resolvers += "Scribe OAuth Library" at "http://repo1.maven.org/maven2/org/scribe/scribe/"
+       
     )
 
 }
